0b34499b6febe3aa13d1c163ae5c9f964601cf15
kraibse <33869366+kraibse@users.noreply.github.com>
2026-06-10 13:44:18 +0200

    Update application namespace and description in configuration files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ping_tracker"
+    namespace = "app.kraibse.ping_tracker"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ping_tracker"
+        applicationId = "app.kraibse.ping_tracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/app/kraibse/ping_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package app.kraibse.ping_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
